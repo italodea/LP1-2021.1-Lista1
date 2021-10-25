a6d335800c830c3f913b15a91e2e79a8aec48f61
Ítalo de Azevedo Albuquerque <italodeazevedo99@gmail.com>
2021-10-25 19:32:16 -0300

    Resolução da questão Minmax

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -10,10 +10,23 @@
  * @return A pair of indexes to the first smallest and last largest values.
  */
 
-std::pair<int,int> min_max( int V[], size_t n )
+std::pair<int,int> min_max( int V[], std::size_t n )
 {
+    if (n == 0) {
+        return {-1, -1};
+    }
     // TODO: Adicione aqui sua solução.
-
+    int menor = V[0], menorPos = 0 , maior = V[n - 1], maiorPos = n -1;
+    for (int i = 0, j = n-1 ; i < n; ++i,--j) {
+        if(V[i] < menor){
+            menor = V[i];
+            menorPos = i;
+        }
+        if(V[j] > maior){
+            maior  = V[j];
+            maiorPos = j;
+        }
+    }
     // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    return { menorPos, maiorPos };
 }
